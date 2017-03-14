6842f7ffbbce13f750fa70c84331bd0ecaae0d44
deep-diver <deep.diver.csp@gmail.com>
2017-03-14 16:54:08 +0900

    fixed #4 - finish up preprocessing step4.

--- rnn.py
+++ rnn.py
@@ -15,12 +15,17 @@ def get_text_from_file(filename):
 
 keep_vocab_freq = 8000
 unknown_token = "UNKNOWN_TOKEN"
+sentence_start_token = "SENTENCE_START"
+sentence_end_token = "SENTENCE_END"
 
 # get raw text from the file, 'anna.txt'
 raw_text = get_text_from_file('anna.txt')
 
 # tokenize raw_text into sentences
 sentences = sent_tokenize(raw_text)
+
+# prepend special tokens (start/end of sentences)
+sentences = ["%s %s %s" % (sentence_start_token, x, sentence_end_token) for x in sentences]
 
 # tokenize each sentences, and stack them into array
 tokenized_sentences = [word_tokenize(sent) for sent in sentences]
@@ -36,3 +41,9 @@ vocab = freq_word.most_common(keep_vocab_freq)
 index_to_word = [x[0] for x in vocab]
 index_to_word.append(unknown_token)
 word_to_index = {c: i for i, c in enumerate(index_to_word)}
+
+# replace infrequent words with special string, unknown_token
+for i, sentence in enumerate(tokenized_sentences):
+    for j, word in enumerate(sentence):
+        if (word not in word_to_index):
+            tokenized_sentences[i][j] = unknown_token
